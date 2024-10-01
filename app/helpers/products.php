14e784419303bd16b8545a581ed3be7354ef32e8
Miguel Vital <miguelvital05@gmail.com>
2024-09-30 21:52:29 -0300

    Link para add o produto no carrinho

---
+++ app/helpers/products.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    1 => ['name' => 'Product1', 'price' => 3400],
+    2 => ['name' => 'Product2', 'price' => 1000],
+    3 => ['name' => 'Product3', 'price' => 4567],
+    4 => ['name' => 'Product4', 'price' => 8437],
+    5 => ['name' => 'Product5', 'price' => 9876],
+    6 => ['name' => 'Product6', 'price' => 2198],
+];
--- public/index.php
+++ public/index.php
@@ -2,4 +2,34 @@
 
 require '../vendor/autoload.php';
 
-echo 'Cart';
+$products = require '../app/helpers/products.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+</head>
+
+<body>
+    <div id="container">
+        <ul>
+                <?php foreach ($products as $index => $product): ?>
+                    <li>
+                        <?php 
+                            echo $product['name'];
+                        ?> | 
+                        R$ <?php
+                             echo number_format($product['price'], 2, ',', '.');
+                        ?>
+                    </li>
+                    <a href="add.php?id=<?php echo $index ?>">Add to Cart</a>
+                <?php endforeach; ?>
+        </ul>
+    </div>
+</body>
+
+</html>
