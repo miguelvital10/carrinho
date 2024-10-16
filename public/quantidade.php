732f2ed2fac8dbfee4c8aa39ef482e556c0ac725
Miguel Vital <miguelvital05@gmail.com>
2024-10-16 16:39:00 -0300

    alterar quantidade do produto no carrinho

---
+++ public/quantidade.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+require '../vendor/autoload.php';
+
+use app\classes\Cart;
+
+$quantity = filter_input(INPUT_GET, 'qty', FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+$cart = new Cart;
+$cart->quantity($id, $quantity);
+
+header('Location:cart.php');
